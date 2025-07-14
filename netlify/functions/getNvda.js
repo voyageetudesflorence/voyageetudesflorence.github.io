60e9bf119036418df63775de7f357dfc273dac0d
Albert <albert.cirstoiu+florence@gmail.com>
2025-07-14 22:51:08 +0300

    Update getNvda.js
    -

--- netlify/functions/getNvda.js
+++ netlify/functions/getNvda.js
@@ -1,5 +1,5 @@
 export async function handler(event, context) {
-  const apiKey = process.env.API_KEY;
+  const apiKey = process.env.TWELVEDATA_API_KEY;
 
   try {
     const response = await fetch(`https://api.twelvedata.com/price?symbol=NVDA&apikey=${apiKey}`);
@@ -8,19 +8,19 @@ export async function handler(event, context) {
     if (data && data.price) {
       return {
         statusCode: 200,
-        body: JSON.stringify({ currentPrice: parseFloat(data.price) })
+        body: JSON.stringify({ currentPrice: parseFloat(data.price) }),
       };
     } else {
       return {
         statusCode: 500,
-        body: JSON.stringify({ error: "Invalid response from Twelve Data" })
+        body: JSON.stringify({ error: "Invalid response from Twelve Data" }),
       };
     }
-
   } catch (err) {
     return {
       statusCode: 500,
-      body: JSON.stringify({ error: "Failed to fetch NVDA price" })
+      body: JSON.stringify({ error: "Failed to fetch NVDA price" }),
     };
   }
 }
+
